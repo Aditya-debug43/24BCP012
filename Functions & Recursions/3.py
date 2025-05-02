54f1a6652c57d6c613cdee06550ea9c70b26f264
___Aditya_ <asharaa899@gmail.com>
2025-05-02 12:24:34 +0530

    Delete Functions & Recursions directory

--- Functions & Recursions/3.py
+++
@@ -1,7 +0,0 @@
-def create_array(n):
-    return [[[n for i in range(5)]for i in range(4)]for i in range(3)]
-
-n = int(input('Enter the number : '))
-print(create_array(n))
-    
-    
--- Functions & Recursions/recursion_example_3.py
+++
@@ -1,6 +0,0 @@
-def apb(a,b):
-    if b == 0:
-        return 1
-    else:
-        return a*(apb(a,b-1))
-print(apb(2,3))
