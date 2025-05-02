77fab42afe9aff80d959d334ef1c4ce4171f7ed0
___Aditya_ <asharaa899@gmail.com>
2025-05-02 12:25:41 +0530

    Delete Tuples directory

--- Tuples/5.py
+++
@@ -1,8 +0,0 @@
-def q5():
-    l=[(),'Ronit',('lst',),[1,2,3],[]]
-    for i in l:
-        if(type(i)==tuple and len(i)==0):
-            l.remove(i)
-        else:
-            pass
-    print(l)
--- Tuples/6.py
+++
@@ -1,6 +0,0 @@
-def q6():
-    t=(1,2,3,5,6,7)
-    t=list(t)
-    t[0]=0
-    t=tuple(t)
-    print(t)
